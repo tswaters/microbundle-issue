f06a9c33dcf34d96716d2d175345929e0a1c5358
Tyler Waters <tyler.waters@gmail.com>
2022-03-27 16:28:20 -0700

    Add node16 browserlist

--- dist/foo.cjs
+++ dist/foo.cjs
@@ -1,23 +1,6 @@
-function _extends() {
-  _extends = Object.assign || function (target) {
-    for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i];
-
-      for (var key in source) {
-        if (Object.prototype.hasOwnProperty.call(source, key)) {
-          target[key] = source[key];
-        }
-      }
-    }
-
-    return target;
+const shallowClone = obj => {
+  return { ...obj
   };
-
-  return _extends.apply(this, arguments);
-}
-
-var shallowClone = function shallowClone(obj) {
-  return _extends({}, obj);
 };
 
 exports.shallowClone = shallowClone;
